1f4571c975000b99f9ea9c8bbaa2aa6e8d1f26c4
Darren Pan <darrenspace@icloud.com>
2025-05-22 06:53:29 +0800

    introduced a sheet to display Detailview

--- GitLearning/ContentView.swift
+++ GitLearning/ContentView.swift
@@ -8,14 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showSheet = false
+    
     var body: some View {
         NavigationStack {
             VStack {
                 NavigationLink(destination: DetailView()) {
                     Text("Hello Detail View")
                 }
+                Button {
+                    showSheet = true
+                } label: {
+                    Text("Show sheet")
+                }
+
             }
             .padding()
+            .sheet(isPresented: $showSheet) {
+                DetailView()
+            }
         }
     }
 }
