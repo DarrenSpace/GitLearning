6aaef1baedfffa069fd3efe752d3227c5bdf1a8e
Darren Pan <darrenspace@icloud.com>
2025-05-21 22:35:14 +0800

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Darren Pan on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
